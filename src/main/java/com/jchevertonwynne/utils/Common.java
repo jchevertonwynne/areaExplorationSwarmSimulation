db6e977427d707b060c886afe8e1f354cb9f0b3d
Joseph Cheverton-Wynne <jchevertonwynne@gmail.com>
2020-03-24 19:56:35 +0000

    all results

--- src/main/java/com/jchevertonwynne/utils/Common.java
+++ src/main/java/com/jchevertonwynne/utils/Common.java
@@ -3,7 +3,7 @@ package com.jchevertonwynne.utils;
 import com.jchevertonwynne.structures.Coord;
 
 public class Common {
-    public static final String BACKGROUND_NAME = "areas/mazeSmall.png";
+    public static final String BACKGROUND_NAME = "areas/mazeSmallLinked.png";
     public static final Coord START_POSITION = new Coord(20, 20);
 
     public static final boolean DISPLAY = true;
@@ -12,9 +12,9 @@ public class Common {
     public static final int DFS_MAX_TURNS_WITHOUT_FIND = 400;
     public static final int DFS_RETURN_SOFT_CAP = 400;
 
-    public static final boolean GLOBAL_KNOWLEDGE = true;
-    public static final boolean EMPLOY_DROPS = false;
-    public static final int AGENT_COUNT = 7;
+    public static final boolean GLOBAL_KNOWLEDGE = false;
+    public static final boolean EMPLOY_DROPS = true;
+    public static final int AGENT_COUNT = 5;
     public static final int SIGHT_RADIUS = 30;
     public static final int BROADCAST_RADIUS = 60;
 }
